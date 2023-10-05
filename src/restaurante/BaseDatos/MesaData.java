0368ecdf6bbd7e3f909f0c260710fc3d12ece5fd
dani35694 <141648320+dani35694@users.noreply.github.com>
2023-10-05 12:15:37 -0300

    actuu

--- src/restaurante/BaseDatos/MesaData.java
+++ src/restaurante/BaseDatos/MesaData.java
@@ -2,13 +2,72 @@
 package restaurante.BaseDatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import restaurante.Entidades.Mesa;
 
 
 public class MesaData {
     
     private Connection con = null;
     
+    public MesaData (){
+        con = Conexion.getConexion();
+    }
     
+    List<Mesa> mesa=new ArrayList<>();
+    Mesa mesas = new Mesa();
+    //public class Mesa {
+//    private int idMesa;
+//    private Reserva reserva;
+//    private Pedido pedido;
+//    private int capacidad;
+//    private boolean estado;
+
+    
+    public void guardarMesa(Mesa mesaP){
+        String sql=" INSER INTO mesa(numero ,capacidad ,estado)VALUES (?,?,?)";
+        
+        try {
+            PreparedStatement ps =con.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1,mesaP.getNumero());
+            ps.setInt(2, mesaP.getCapacidad());
+            ps.setBoolean(3, mesaP.isEstado());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                mesaP.setIdMesa(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, " Producto Guardado ");
+                
+            }
+            
+       
+            ps.close();
+            
+            
+            
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al Actualizar Tabla de Mesas "+ex.getMessage());
+
+        }
+       
+        
+        
+    }
+
+    
+    
+
     
 
     
--- src/restaurante/Entidades/Mesa.java
+++ src/restaurante/Entidades/Mesa.java
@@ -5,19 +5,22 @@ public class Mesa {
     private int idMesa;
     private Reserva reserva;
     private Pedido pedido;
+    private int numero;
     private int capacidad;
     private boolean estado;
 
-    public Mesa(int idMesa, Reserva reserva, Pedido pedido, int capacidad, boolean estado) {
+    public Mesa(int idMesa, Reserva reserva,int numero, Pedido pedido, int capacidad, boolean estado) {
         this.idMesa = idMesa;
         this.reserva = reserva;
+        this.numero=numero;
         this.pedido = pedido;
         this.capacidad = capacidad;
         this.estado = estado;
     }
 
-    public Mesa(Reserva reserva, Pedido pedido, int capacidad, boolean estado) {
+    public Mesa(Reserva reserva,int numero, Pedido pedido, int capacidad, boolean estado) {
         this.reserva = reserva;
+        this.numero=numero;
         this.pedido = pedido;
         this.capacidad = capacidad;
         this.estado = estado;
@@ -26,15 +29,6 @@ public class Mesa {
     public Mesa(){
     }
 
-    public int getCapacidad() {
-        return capacidad;
-    }
-
-    public void setCapacidad(int capacidad) {
-        this.capacidad = capacidad;
-    }
-
-    
     public int getIdMesa() {
         return idMesa;
     }
@@ -59,6 +53,22 @@ public class Mesa {
         this.pedido = pedido;
     }
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
     public boolean isEstado() {
         return estado;
     }
@@ -69,7 +79,9 @@ public class Mesa {
 
     @Override
     public String toString() {
-        return "Mesa{" + "idMesa=" + idMesa + ", reserva=" + reserva + ", pedido=" + pedido + ", capacidad=" + capacidad + ", estado=" + estado + '}';
+        return "Mesa{" + "idMesa=" + idMesa + ", reserva=" + reserva + ", pedido=" + pedido + ", numero=" + numero + ", capacidad=" + capacidad + ", estado=" + estado + '}';
     }
+
+
  
 }
